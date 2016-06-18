f1a70d102926d5b9894b5e90400b737f6bd1c5cf
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-06-18 02:26:21 +0000

    connect to db using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var morgan = require("morgan");
+var mongoose = require("mongoose");
 
 var app = express();
+
+mongoose.connect("mongodb://getnpk:getnpk@ds017544.mlab.com:17544/ecommerce", function(err){
+    if (err){
+        console.log(err);
+    } else {
+        console.log("Connected to DB!");
+    }
+});
 
 app.use(morgan("dev"));
 
