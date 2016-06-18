dce87cc0bec7634e17f2dd644b3494599f896dcd
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-06-18 05:50:39 +0000

    moving to routers

---
+++ routes/user.js
@@ -0,0 +1,29 @@
+var router = require("express").Router();
+var User = require("../models/user");
+
+router.post("/signup", function(req, res, next) {
+    var user = new User();
+    user.profile.name = req.body.name;
+    user.password = req.body.password;
+    user.email = req.body.email;
+
+    User.findOne({
+        email: req.body.email
+    }, function(err, existingUser) {
+        if (existingUser) {
+            console.log("The user exists: " + req.body.email);
+            return res.redirect("/signup");
+        }
+        else {
+            user.save(function(err) {
+                if (err) return next(err);
+                res.json({
+                    "message": "Successfully created user " + user.profile.name
+                });
+            });
+        }
+    });
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -26,25 +26,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.engine("ejs", engine);
 app.set("view engine", "ejs");
 
-app.post("/create-user", function(req, res, next){
-    var user = new User();
-    user.profile.name = req.body.name;
-    user.password = req.body.password;
-    user.email = req.body.email;
-    
-    user.save(function(err){
-       if (err) return next(err);
-       res.json({"message": "Successfully saved user " + user.profile.name});
-    });
-});
-
-app.get("/", function(req, res){
-   res.render("main/home"); 
-});
-
-app.get("/about", function(req, res){
-   res.render("main/about"); 
-});
+//Routers
+var mainRoutes = require("./routes/main");
+app.use(mainRoutes);
+var userRoutes = require("./routes/user");
+app.use(userRoutes);
 
 app.listen("8080", function(err){
    if (err) throw err;
