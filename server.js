97f4ff1d397302fc75c87418c06bd2a0afb73306
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-06-18 04:16:04 +0000

    ejs-mate, boilerplate and partials

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var morgan = require("morgan");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var ejs = require("ejs");
+var engine = require("ejs-mate");
 
 var User = require("./models/user.js");
 
@@ -20,6 +22,9 @@ app.use(morgan("dev"));
 //app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
+
 app.post("/create-user", function(req, res, next){
     var user = new User();
     user.profile.name = req.body.name;
@@ -30,6 +35,14 @@ app.post("/create-user", function(req, res, next){
        if (err) return next(err);
        res.json({"message": "Successfully saved user " + user.profile.name});
     });
+});
+
+app.get("/", function(req, res){
+   res.render("main/home"); 
+});
+
+app.get("/about", function(req, res){
+   res.render("main/about"); 
 });
 
 app.listen("8080", function(err){
