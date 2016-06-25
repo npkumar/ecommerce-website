e68a75a8e6fa6356dd9e3ee72911a25f87d82c75
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-06-25 04:09:09 +0000

    add gravatar

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var crypto = require("crypto");
 var Schema = mongoose.Schema;
 var bcrypt = require("bcrypt-nodejs");
 
@@ -39,6 +40,13 @@ UserSchema.pre("save", function(next){
 // compare password with that in db
 UserSchema.methods.comparePassword = function(password){
     return bcrypt.compareSync(password, this.password);
+}
+
+UserSchema.methods.gravatar = function(size){
+    if (!this.size) size = 200;
+    if (!this.email) return "https://gravatar.com/avatar/?s" + size + "&d=retro";
+    var md5 = crypto.createHash("md5").update(this.email).digest("hex");
+    return "https://gravatar.com/avatar/" + md5 + "?s=" + size + "&d=retro";
 }
 
 module.exports = mongoose.model("User", UserSchema);
