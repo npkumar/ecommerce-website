451792b9a021b2daa418769b971904656be3c353
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-06-27 07:12:47 +0000

    populate categories page based on product id

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,7 @@
 var router = require("express").Router();
 
+var User = require("../models/user");
+var Product = require("../models/product");
 
 router.get("/", function(req, res){
    res.render("main/home"); 
@@ -7,6 +9,26 @@ router.get("/", function(req, res){
 
 router.get("/about", function(req, res){
    res.render("main/about"); 
+});
+
+// for testing
+router.get("/users", function(req, res){
+   User.find({}, function(err, users){
+      if (err) return res.json({"message": err});
+      return res.json(users);
+   });
+});
+
+router.get("/products/:id", function(req, res, next){
+   Product
+   .find({category: req.params.id})
+   .populate("category")
+   .exec(function(err, products){
+      if (err) return next(err);
+      res.render("main/category", {
+         products: products
+      });
+   });
 });
 
 module.exports = router;
