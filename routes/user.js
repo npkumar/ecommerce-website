6efe8219c6003c911b7a479a1427f4682fb68397
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-06-27 01:32:06 +0000

    add routes for edit profile

--- routes/user.js
+++ routes/user.js
@@ -21,6 +21,25 @@ router.get("/logout", function(req, res) {
     res.redirect("/");
 });
 
+router.get("/edit-profile", function(req, res, next) {
+    res.render("accounts/edit-profile", { message: req.flash("success")});
+});
+
+router.post("/edit-profile", function(req, res, next) {
+    User.findOne({_id: req.user._id}, function(err, user) {
+       if (err) return next(err);
+       
+       if (req.body.name) user.profile.name = req.body.name;
+       if (req.body.address) user.profile.address = req.body.address;
+       
+       user.save(function(err){
+           if (err) return next(err);
+           req.flash("success", "Successfully edited your profile");
+           return res.redirect("/edit-profile");
+       });
+    });
+});
+
 router.get("/profile", function(req, res, next) {
     User.findOne({_id: req.user._id}, function(err, user) {
         if (err) return next(err);
