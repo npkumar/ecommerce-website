dd9342a2feb8cd6b7ea7e09646d43985dafb19d6
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-06-27 01:53:00 +0000

    view for sign up page

--- routes/user.js
+++ routes/user.js
@@ -30,7 +30,7 @@ router.post("/edit-profile", function(req, res, next) {
        if (err) return next(err);
        
        if (req.body.name) user.profile.name = req.body.name;
-       if (req.body.address) user.profile.address = req.body.address;
+       if (req.body.address) user.address = req.body.address;
        
        user.save(function(err){
            if (err) return next(err);
